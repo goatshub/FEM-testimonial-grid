4abc238fd282049bae9d04a17b1226e9ceaca7b1
goatshub <kiatwongwanich@gmail.com>
2023-06-16 17:34:06 +0700

    update icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Barlow_Semi_Condensed } from 'next/font/google'
 import CardGrid from '../components/cardGrid'
+import Head from 'next/head';
 const barlow = Barlow_Semi_Condensed({ subsets: ['latin'], weight: ["500", "600"] })
 
 export default function Home() {
@@ -7,6 +8,10 @@ export default function Home() {
     <main
       className={`grid min-h-screen place-items-center px-5 py-16 ${barlow.className}`}
     >
+      <Head>
+        <title>Testimonials Grid</title>
+        <link rel="icon" href="/images/favicon-32x32.png" />
+      </Head>
       <CardGrid />
     </main>
   )

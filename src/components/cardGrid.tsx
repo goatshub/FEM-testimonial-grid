3b920fa88420a07a465bb5921fd8078358659ded
goatshub <kiatwongwanich@gmail.com>
2023-06-16 17:26:49 +0700

    First commit

---
+++ src/components/cardGrid.tsx
@@ -0,0 +1,13 @@
+import Card from './card'
+import { reviewUsers } from '@/reviewUsers'
+import cardGridStyles from './cardGrid.module.scss'
+const CardGrid = () => {
+  return (
+    <section className={`${cardGridStyles.cardGrid}`}>
+        {reviewUsers.map((reviewUser,key) => 
+                <Card key={key} {...reviewUser } />
+        )}
+    </section>
+  )
+}
+export default CardGrid
